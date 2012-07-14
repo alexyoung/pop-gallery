8230bc0b08804c27ca3c30aed87114821940833c
Alex Young <alex@helicoid.net>
2012-07-14 20:15:16 +0100

    Updated path API to work with the new Node

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 var assert = require('assert')
   , gallery = require(__dirname + '/../lib/index')
-
-var result = gallery.helpers.gallery.apply({ root: __dirname + '/fixtures/' });
+  , result = gallery.helpers.gallery.apply({ root: __dirname + '/fixtures/' });
 
 // Make sure it actually finds the files
 assert.ok(result.match('div id="gallery"'));
